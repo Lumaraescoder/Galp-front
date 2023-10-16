b352480a07d808fcc05514fa6a51d965ecb86e08
lu_maraes <50714793+Lumaraescoder@users.noreply.github.com>
2023-10-16 14:49:23 +0100

    refinings

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,21 +1,22 @@
 import Link from 'next/link';
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 
-import { Logo, MenuIcon, MenuLinks, NavbarContainer, StyledLink } from './NavigationStyled';
-
-type ActiveLink = 'Home' | 'BackOffice';
+import {
+  ActiveLink,
+  Logo,
+  MenuIcon,
+  MenuLinks,
+  NavbarContainer,
+  StyledLink
+} from './NavigationStyled';
 
 const Navigation = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isOpen, setIsOpen] = useState(false);
   const [activeLink, setActiveLink] = useState<ActiveLink>('Home');
 
-  const handleSetActiveLink = useCallback((link: ActiveLink) => {
+  const handleSetActiveLink = (link: ActiveLink) => {
     setActiveLink(link);
-  }, []);
-
-  const toggleMenu = useCallback(() => {
-    setIsOpen((prevIsOpen) => !prevIsOpen);
-  }, []);
+  };
 
   return (
     <NavbarContainer>
@@ -42,23 +43,24 @@ const Navigation = () => {
           </div>
         </div>
 
-        {/* Container central para os links de navegação */}
         <MenuLinks open={isOpen} className="ml-auto space-x-4 md:flex">
           <div className="flex items-center space-x-2">
-            <i className="fa fa-user" aria-hidden="true"></i>
+            &nbsp;
+            <i className="fa fa-user-circle"></i>
+            &nbsp;
             <Link href="/#">João</Link>
+            &nbsp;
             <i className="fa fa-caret-down" aria-hidden="true"></i>
           </div>
         </MenuLinks>
 
         <div className="flex items-center">
-          <MenuIcon onClick={toggleMenu} className="mr-4 block cursor-pointer md:hidden">
+          <MenuIcon
+            onClick={() => setIsOpen(!isOpen)}
+            className="mr-4 block cursor-pointer md:hidden"
+          >
             <i className="fa fa-bars" aria-hidden="true"></i>
           </MenuIcon>
-
-          <div className="block md:hidden">
-            <Logo src="" alt="Galp logo" />
-          </div>
         </div>
       </div>
     </NavbarContainer>
--- src/components/Navigation/NavigationStyled.tsx
+++ src/components/Navigation/NavigationStyled.tsx
@@ -1,47 +1,45 @@
 import styled from 'styled-components';
 
-type StyledLinkProps = {
-  active: boolean; // Não é opcional desta vez, pois estamos passando explicitamente
-} & React.AnchorHTMLAttributes<HTMLAnchorElement>;
-type MenuLinksProps = {
+export type MenuLinksProps = {
   open: boolean;
 };
+
+export type StyledLinkProps = {
+  active: boolean;
+  href: string;
+  onClick: () => void;
+  className: string;
+};
+export type ActiveLink = 'Home' | 'BackOffice';
+
 export const NavbarContainer = styled.nav`
-  /* Seus estilos personalizados aqui */
   background-color: #ea5b0b;
   padding: 1rem;
   color: white;
 `;
 
 export const Logo = styled.img`
-  height: 50px; /* Ou outro tamanho conforme necessário */
+  height: 50px;
 `;
 
 export const MenuIcon = styled.button`
-  /* Estilos para o ícone do menu */
   @media (min-width: 1024px) {
     display: none;
   }
 `;
 
 export const MenuLinks = styled.div<MenuLinksProps>`
-  /* seus estilos */
-
-  display: none; /* por padrão será ocultado */
-
   @media (max-width: 1023px) {
     display: ${(props) => (props.open ? 'block' : 'none')};
   }
 `;
 export const StyledLink = styled.a<StyledLinkProps>`
-  /* ... estilos existentes ... */
   border-bottom: 3px solid transparent;
   transition: border-bottom-color 0.3s ease-in;
 
-  /* Estilização baseada na propriedade 'active' */
   border-bottom-color: ${(props) => (props.active ? 'currentColor' : 'transparent')};
 
   &:hover {
-    border-bottom-color: currentColor; /* A cor atual do texto */
+    border-bottom-color: currentColor;
   }
 `;
