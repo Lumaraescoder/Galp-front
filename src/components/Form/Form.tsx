c26879e6e7cd7b720a238b0c92bed693989559bb
lu_maraes <50714793+Lumaraescoder@users.noreply.github.com>
2023-10-18 16:17:02 +0100

    BackOffice

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,28 +2,25 @@ import React from 'react';
 import styled from 'styled-components';
 
 const RightSection = styled.div`
-  flex: 0 0 45%;
-  /* background: #f5f5f5; // changed background color to a light grey to match the screenshot */
-  border-radius: 8px;
-  margin-top: 211px;
-  padding: 20px; // added padding for the entire container
+  flex: 0 0 100%;
+  border-radius: 3px;
+  margin-left: 60px;
+  margin-top: 240px;
+  @media (min-width: 768px) {
+    flex: 0 0 45%;
+  }
+  @media (max-width: 768px) {
+    margin-top: 60px;
+    margin-left: 0;
+    padding-left: 10px;
+  }
 `;
 
-const FileName = styled.span`
-  /* margin-left: 15px; */
-`;
-
-const RightSideFormContainer = styled.div`
-  padding: 20px;
-`;
+const RightSideFormContainer = styled.div``;
 
 const InputContainer = styled.div`
-  /* margin-bottom: 10px; */
-  margin-bottom: 20px; // increased margin for spacing
-`;
-
-const UploadIcon = styled.span`
-  margin-right: 10px;
+  margin-bottom: 20px;
+  width: 100%;
 `;
 
 const HiddenInput = styled.input`
@@ -34,7 +31,24 @@ const StyledTextArea = styled.textarea`
   width: 100%;
   padding: 12px 15px;
   resize: vertical;
-  margin-bottom: 15px;
+  resize: none;
+  background-color: #f5f5f5;
+  ::placeholder {
+    color: black;
+    opacity: 1;
+  }
+`;
+
+const LogoButton = styled.div`
+  display: flex;
+  align-items: center;
+  border-radius: 8px;
+  color: black;
+  padding: 10px 15px;
+  cursor: pointer;
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 const LogoUploads = styled.div`
@@ -44,66 +58,121 @@ const LogoUploads = styled.div`
 `;
 
 const UploadedLogo = styled.img`
-  width: 50px; // reduced size to match the screenshot
-  border: 1px solid #e0e0e0; // added border
-  border-radius: 8px; // increased border-radius
+  width: 50px;
+  border: 1px solid #e0e0e0;
+  border-radius: 8px;
 `;
+
 const ButtonsContainer = styled.div`
   display: flex;
   flex-direction: column;
-
   @media (min-width: 768px) {
     flex-direction: row;
     justify-content: space-between;
+    gap: 10px;
   }
 `;
 
 const Button = styled.button`
   padding: 12px 20px;
   cursor: pointer;
-  border: 1px solid #e0e0e0; // changed border color
-  border-radius: 8px; // increased border-radius
-  font-weight: bold; // added font-weight
+  border: 1px solid #e0e0e0;
+  border-radius: 8px;
+  font-weight: bold;
+  @media (max-width: 768px) {
+    width: 100%;
+    margin-bottom: 10px;
+  }
 `;
 
 const CancelButton = styled(Button)`
   background-color: #ffffff;
-  color: #666; // added text color
+  color: #666;
   width: 45%;
   border: solid 1px #ff6347;
   color: #ff6347;
+  @media (max-width: 768px) {
+    width: 100%;
+    margin-bottom: 10px;
+  }
 `;
 
 const SubmitButton = styled(Button)`
   background-color: #ff6347;
   color: #ffffff;
   width: 45%;
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 const StyledLabel = styled.label`
-  display: block; // changed to block for better alignment
-  margin-bottom: 10px; // added margin for spacing
-  font-weight: bold; // added font-weight to make it look like the screenshot
-  color: #666; // changed color to a dark grey to match the screenshot
+  display: block;
+  margin-bottom: 10px;
+  font-weight: 400;
+  color: black;
+  font-size: 16px;
 `;
 
 const StyledInput = styled.input`
   width: 100%;
   padding: 12px 15px;
-  border: 1px solid #e0e0e0; // changed border color to a light grey
-  border-radius: 8px; // increased border-radius
-`;
-const TwoColumns = styled.div`
-  display: flex;
-  gap: 10px;
-
+  border: none;
+  background-color: #f5f5f5;
+  ::placeholder {
+    color: black !important;
+    opacity: 1;
+  }
   @media (max-width: 768px) {
-    flex-direction: column;
+    width: 100%;
+    margin: 5px 0;
   }
 `;
 
+const TwoColumns = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
 const StyledInputFull = styled.input`
+  background-color: white;
   flex: 1;
+`;
+
+const StyledInputFullRight = styled.input`
+  background-color: #f5f5f5;
+  flex: 1;
+  width: 100%;
+  padding: 0px 10px;
+`;
+
+const Icon = styled.i`
+  position: absolute;
+  left: 10px;
+  top: 50%;
+  color: #ea5b0b !important;
+  transform: translateY(-50%);
+  font-size: 21px;
+`;
+
+const IconButton = styled.i`
+  color: #ea5b0b !important;
+  transform: translateY(-50%);
+  font-size: 31px;
+  position: relative;
+  top: 15px;
+`;
+
+const InputIconContainer = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  background-color: #f5f5f5;
+  border-radius: 8px;
+`;
+
+const StyledInputWithIcon = styled(StyledInputFull)`
+  padding-left: 40px;
 `;
 
 const Form = () => {
@@ -113,15 +182,16 @@ const Form = () => {
         <TwoColumns>
           <InputContainer>
             <StyledLabel>Contract </StyledLabel>
-            <StyledInputFull type="text" placeholder="Upload Contract" />
+            <InputIconContainer>
+              <Icon className="fa fa-cloud-upload" aria-hidden="true"></Icon>
+              <StyledInputWithIcon type="text" placeholder="Upload Contract" />
+            </InputIconContainer>
           </InputContainer>
-
           <InputContainer>
             <StyledLabel>&nbsp;</StyledLabel>
-            <StyledInputFull type="text" placeholder="contract.pdf" />
+            <StyledInputFullRight type="text" placeholder="contract.pdf" />
           </InputContainer>
         </TwoColumns>
-
         <InputContainer>
           <StyledLabel>Contract Date</StyledLabel>
           <StyledInput type="text" placeholder="10/10/2021" />
@@ -130,20 +200,20 @@ const Form = () => {
           <StyledLabel>Cashflow</StyledLabel>
           <StyledInput type="text" placeholder="$5B" />
         </InputContainer>
-
         <InputContainer>
           <StyledLabel>Description</StyledLabel>
           <StyledTextArea placeholder="..."></StyledTextArea>
         </InputContainer>
-
         <InputContainer as={LogoUploads}>
           <StyledLabel as="div">
-            <UploadIcon>🔼</UploadIcon> Upload Logo
-            <HiddenInput type="file" />
+            <LogoButton onClick={() => document.getElementById('fileInput').click()}>
+              <IconButton className="fa fa-cloud-upload" aria-hidden="true"></IconButton>
+              &nbsp; Upload Logo
+            </LogoButton>
+            <HiddenInput id="fileInput" type="file" />
           </StyledLabel>
-          <UploadedLogo src="/path/to/logo.png" alt="Uploaded Logo" />
+          <UploadedLogo id="uploadedLogo" src="images/logo-form.png" alt="Uploaded Logo" />
         </InputContainer>
-
         <ButtonsContainer>
           <CancelButton>Cancel</CancelButton>
           <SubmitButton>Submit</SubmitButton>
--- src/pages/backoffice.tsx
+++ src/pages/backoffice.tsx
@@ -1,51 +1,66 @@
-import React from 'React';
 import styled from 'styled-components';
 
 import Form from '@/components/Form/Form';
 
 const StyledInput = styled.input`
-  width: 90%;
-  padding: 12px 15px; // Increased padding
-  // Increased margin-bottom
+  width: 100%;
+  padding: 12px 15px;
   border: 1px solid #ccc;
   border-radius: 5px;
+  background-color: #f5f5f5;
+  border: none;
+  margin-top: 5px;
+  @media (max-width: 768px) {
+    font-size: larger;
+  }
 `;
 
 const Container = styled.div`
   max-width: 1200px;
   margin: 0 auto;
-  padding: 20px;
   padding: 40px 20px;
   display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
+  justify-content: center;
+  background-color: #ffffff;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 20px;
+    align-items: stretch;
+  }
 `;
 
 const LeftSection = styled.div`
-  flex: 0 0 60%;
-  max-width: 50%;
+  flex: 1;
+
+  @media (max-width: 768px) {
+    max-width: 100%;
+    margin-bottom: 20px;
+  }
 `;
 
 const Flex = styled.div`
   display: flex;
   gap: 20px;
-
-  @media (max-width: 768px) {
-    flex-direction: column;
-  }
 `;
 
 const StyledLabel = styled.label`
   display: flex;
   align-items: center;
   cursor: pointer;
+  white-space: nowrap;
+
+  @media (max-width: 768px) {
+    white-space: normal;
+  }
 `;
 
 const Image = styled.img`
-  width: 120px; // Adjusted width
-  height: 120px; // Adjusted height
-  margin: 15px 40px;
+  width: 120px;
+  height: 120px;
+  margin: 3px 0px;
 `;
+
 const ContainerHeader = styled.div`
   display: flex;
   align-items: center;
@@ -53,39 +68,45 @@ const ContainerHeader = styled.div`
   margin-top: -11px;
   p {
     color: #030517;
-    font-size: 36px; // Adjusted font-size
     font-weight: 799;
   }
 `;
 
 const ButtonStake = styled.button`
-  padding: 10px 20px;
   border: none;
   background-color: E6E6E6;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  margin-right: 10px; // For spacing between buttons
+  margin-right: 10px;
   cursor: pointer;
   transition: background-color 0.3s ease;
   margin: 0px 53px;
   border-radius: 8px;
   font-weight: 503;
-  width: 221px;
+  width: 50%;
+
   &:hover {
     background-color: #f0f0f0;
   }
+
+  @media (max-width: 768px) {
+    width: 100%;
+    margin-bottom: 10px;
+  }
 `;
+
 const ButtonDash = styled.button`
   padding: 10px 20px;
   border: none;
   height: 50px;
   background-color: #ffffff;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  margin-right: 10px; // For spacing between buttons
+  margin-right: 10px;
   cursor: pointer;
   transition: background-color 0.3s ease;
   border-radius: 8px;
   font-weight: 503;
-  width: 221px;
+  width: 50%;
+
   &:hover {
     background-color: #f0f0f0;
   }
@@ -93,35 +114,47 @@ const ButtonDash = styled.button`
 
 const ButtonsContainers = styled.div`
   display: flex;
-  margin-top: 20px; // For spacing above the buttons
+  margin-top: 20px;
 `;
 
 const LabelLeft = styled.div`
   margin: 12px 0px;
-`;
-const LabelRight = styled.div`
-  /* margin: 12px 0px; */
-  margin: 12px 20px;
-`;
-const ContainerLeft = styled.div`
-  p {
-    margin: 7px 0px;
-    white-space: nowrap;
+
+  @media (max-width: 768px) {
+    margin-bottom: 10px;
   }
 `;
+
+const LabelRight = styled.div`
+  margin: 12px 20px;
+`;
+
+const WelcomeStack = styled.p`
+  margin: 50px 0px;
+  white-space: nowrap;
+  font-size: 30px;
+  position: relative;
+  top: 24px;
+`;
+
+const ContainerLeft = styled.div``;
+
+const StrackeHolderType = styled.div`
+  margin-top: 29px;
+`;
+
 const LeftSide = () => {
   return (
     <ContainerLeft>
       <ContainerHeader>
         <Image src="/images/boy.png" alt="Boy" />
-        <p>Welcome João</p>
+        <WelcomeStack>Welcome, João !</WelcomeStack>
       </ContainerHeader>
       <ButtonsContainers>
         <ButtonDash>Dashboard</ButtonDash>
         <ButtonStake>Stakeholder</ButtonStake>
       </ButtonsContainers>
-
-      <p>Stackeholder Type</p>
+      <StrackeHolderType>Stackeholder Type</StrackeHolderType>
       <Flex>
         <LabelLeft>
           <input type="radio" name="stakeholder" id="company" />
@@ -132,19 +165,14 @@ const LeftSide = () => {
           <label htmlFor="people"> &nbsp;People</label>
         </LabelRight>
       </Flex>
-
       <StyledLabel htmlFor="stakeholder-name">Stakeholder Name</StyledLabel>
       <StyledInput id="stakeholder-name" type="text" />
-
       <StyledLabel htmlFor="business-name">Business</StyledLabel>
       <StyledInput id="business-name" type="text" />
-
       <StyledLabel htmlFor="location">Location</StyledLabel>
       <StyledInput id="location" type="text" />
-
       <StyledLabel htmlFor="ceo">CEO</StyledLabel>
       <StyledInput id="ceo" type="text" />
-
       <StyledLabel htmlFor="contact">Contact</StyledLabel>
       <StyledInput id="contact" type="text" />
     </ContainerLeft>
