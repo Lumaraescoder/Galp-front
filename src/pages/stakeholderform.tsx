384cfcd478b22e4b4ec07e30e0a4857961bb6639
lu_maraes <50714793+Lumaraescoder@users.noreply.github.com>
2023-10-31 14:46:24 +0000

    keyframes

--- src/pages/stakeholderform.tsx
+++ src/pages/stakeholderform.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 import { useEffect, useRef, useState } from 'react';
 import { StakeholderData } from 'src/types/types';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
 import Spinner from '@/components/Spinner/Spinner';
 
